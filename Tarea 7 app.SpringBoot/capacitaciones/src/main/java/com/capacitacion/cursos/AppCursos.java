d9a14df052b80b0ab33963f06cf7d4634ac61186
Micaela <mickaelabossio@hotmail.com>
2022-11-13 21:23:38 -0300

    practicando con SpringBoot, creando app Capacitaciones

---
+++ Tarea 7 app.SpringBoot/capacitaciones/src/main/java/com/capacitacion/cursos/AppCursos.java
@@ -0,0 +1,13 @@
+package com.capacitacion.cursos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppCursos {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppCursos.class, args);
+	}
+
+}
