5f1a727a6fafc18828393cbdbdd4c8d5852cd4dc
Micaela <mickaelabossio@hotmail.com>
2022-11-27 01:51:15 -0300

    front app Cursos con firebase

--- Tarea 7  SpringBoot/frontcapacitaciones/src/app/services/curso.service.ts
+++ Tarea 7  SpringBoot/frontcapacitaciones/src/app/services/curso.service.ts
@@ -10,7 +10,8 @@ import { Curso } from '../model/curso';
 })
 export class CursoService {
 
-  URL="http://localhost:8080/curso/"
+  // URL="http://localhost:8080/curso/"
+  URL="appcursosrailway-production.up.railway.app/curso"
 
   constructor(private httpClient: HttpClient) { }
 
